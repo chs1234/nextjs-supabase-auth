6ce7eeeb9e984319e9712e080d63d66146caf149
chs1234 <chs4413@gmail.com>
2024-12-01 13:57:07 +0900

    nextjs with supabase boilerplate

---
+++ app/auth/page.tsx
@@ -0,0 +1,31 @@
+import { Button } from '@/components/ui/button'
+import { Github, KeyRound } from 'lucide-react'
+import React from 'react'
+import { FaGithubSquare, FaGoogle } from "react-icons/fa";
+
+export default function Auth() {
+  return (
+    <div className='flex items-center justify-center w-full h-screen'>
+        <div className='w-96 rounded-md border p-5 space-y-5 relative bg-slate-900'>
+            <div className='flex items-center gap-2'>
+                <KeyRound />
+                <h1 className='text-2xl font-bold'>Next + Supabase</h1>
+            </div>
+            
+            <p className='text-sm text-gray-300'>Register/SignIn today 👇</p>
+
+            <div className='flex flex-col gap-5'>
+                <Button className='w-full flex items-center gap-2' variant='outline'>
+                    <FaGithubSquare /> Github
+                </Button>
+                <Button className='w-full flex items-center gap-2' variant='outline'>
+                    <FaGoogle /> Google
+                </Button>
+            </div>      
+
+            <div className='glowBox -z-10'>
+            </div>  
+        </div>
+    </div>
+  )
+}
